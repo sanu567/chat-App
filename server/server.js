fbb4c721a81e55ae0b390e336dec6e504ecd9117
SANU SINGH <sanusingh5607@gmail.com>
2025-12-15 13:04:10 +0530

    Update CORS origin for production environment

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const port=process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-frontend-6gb2.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "token"]
